b5f9761e0bdaf3b36f9ec043a4d0b621189e7c62
Ethan Chee <ethancheez@gmail.com>
2022-11-17 20:22:46 -1000

    Artemis Ground Initial Commit

---
+++ lib/channels/rfm23_channel.cpp
@@ -0,0 +1,42 @@
+#include <artemis_channels.h>
+
+namespace
+{
+    Artemis::Teensy::Radio::RFM23 rfm23(RFM23_CS_PIN, RFM23_INT_PIN, hardware_spi1);
+    PacketComm packet;
+}
+
+void Artemis::Teensy::Channels::rfm23_channel()
+{
+    while (!rfm23.init())
+        ;
+
+    while (true)
+    {
+        if (PullQueue(packet, rfm23_queue, rfm23_queue_mtx))
+        {
+            switch (packet.header.type)
+            {
+            case PacketComm::TypeId::CommandPing:
+            {
+                rfm23.send(packet);
+                threads.delay(500);
+                break;
+            }
+            default:
+                break;
+            }
+        }
+
+        if (rfm23.recv(packet))
+        {
+            Serial.print("[RFM23] RECEIVED: [");
+            for (size_t i = 0; i < packet.data.size(); i++)
+            {
+                Serial.print(packet.data[i], HEX);
+            }
+            Serial.println("]");
+        }
+        threads.delay(10);
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,135 @@
+#include <Arduino.h>
+#include <vector>
+#include <map>
+#include <string.h>
+#include <artemis_channels.h>
+#include <support/configCosmosKernel.h>
+
+void handle_cmd();
+
+namespace
+{
+  char buf[256];
+  PacketComm packet;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(3000);
+
+  threads.setSliceMillis(10);
+
+  // Threads
+  thread_list.push_back({threads.addThread(Artemis::Teensy::Channels::rfm23_channel), "rfm23 thread"});
+}
+
+void loop()
+{
+  handle_cmd();
+}
+
+void handle_cmd()
+{
+  if (Serial.available() > 0)
+  {
+    String input = Serial.readStringUntil('\n');
+    char *cmdline = input.begin();
+
+    vector<string> args;
+
+    for (char *token = strtok(cmdline, " "); token != NULL; token = strtok(NULL, " "))
+    {
+      args.push_back(string(token));
+    }
+
+    if (args.size() < 3)
+    {
+      Serial.println("COMMAND SYNTAX: <node_dest> <radio_out>:<radio_in> <command> [data]");
+      return;
+    }
+
+    // Clear packet
+    packet.data.resize(0);
+    packet.wrapped.resize(0);
+
+    // Set origin
+    packet.header.orig = NODES::GROUND_NODE_ID;
+
+    // Get destination
+    if (NodeType.find(args[0]) != NodeType.end())
+    {
+      packet.header.dest = NodeType.find(args[0])->second;
+    }
+    else
+    {
+      Serial.println("Invalid Node Destination. Options=[artemis_teensy, artemis_rpi]");
+      return;
+    }
+
+    // Parse radio_out and radio_in
+    if (args[1].find(':') == string::npos)
+    {
+      Serial.println("Invalid radio argument format: <radio_out>:<radio_in>");
+      return;
+    }
+    const char *radios = args[1].c_str();
+    char *radio_arg = strtok((char *)radios, ":");
+    String radio_out(radio_arg);
+    radio_arg = strtok(NULL, "");
+    String radio_in(radio_arg);
+    radio_in = radio_in.toLowerCase();
+    if (RadioType.find(radio_in.c_str()) != RadioType.end())
+    {
+      packet.header.radio = RadioType.find(radio_in.c_str())->second;
+    }
+    else
+    {
+      sprintf(buf, "Invalid radio_in: %s", radio_in.c_str());
+      Serial.println(buf);
+      memset(buf, 0, sizeof(buf));
+      return;
+    }
+
+    // Parse command
+    if (packet.StringType.find(args[2]) != packet.StringType.end())
+    {
+      packet.header.type = packet.StringType.find(args[2])->second;
+    }
+    else
+    {
+      Serial.println("Invalid Command Type");
+      return;
+    }
+
+    // Store data
+    if (args.size() > 3)
+    {
+      for (size_t i = 3; i < args.size(); i++)
+      {
+        for (size_t j = 0; j < args[i].size(); j++)
+        {
+          packet.data.push_back(args[i][j]);
+        }
+        if (i != args.size() - 1)
+          packet.data.push_back(' ');
+      }
+    }
+
+    // Send to ground station radio
+    radio_out = radio_out.toLowerCase();
+    if (RadioType.find(radio_out.c_str()) != RadioType.end())
+    {
+      PushQueue(packet, rfm23_queue, rfm23_queue_mtx);
+    }
+    else
+    {
+      sprintf(buf, "Invalid radio_out: %s", radio_out.c_str());
+      Serial.println(buf);
+      memset(buf, 0, sizeof(buf));
+      return;
+    }
+
+    Serial.println("Command sent to radio. Waiting to send...");
+  }
+}
